033b6a3c3bcf26664bcf265a3b88fd6452aed767
Ganesh Murali <ganesh.murali@anywhere.co>
2023-06-20 18:11:43 +0530

    Add Navbar

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,14 +1,13 @@
 const wrapper = document.querySelector(".card-container");
 const dots = document.querySelectorAll(".dot");
 let activeNum = 0;
-
+var cardWidth = document.querySelector('.card').clientWidth;
 dots.forEach((dot, index) => {
     dot.setAttribute("data-num", index);
     dot.addEventListener("click", (e) => {
         let currentDotNumber = e.target.dataset.num;
         if (currentDotNumber !== activeNum) {
-            const pixels = -350 * currentDotNumber;
-            console.log(pixels);
+            const pixels = -cardWidth * currentDotNumber;
             wrapper.style.transform = 'translateX(' + pixels + 'px)';
             var listCards = document.querySelector(".active");
             var listdots = document.querySelector(".activedot");
@@ -25,4 +24,31 @@ dots.forEach((dot, index) => {
             activeNum = currentDotNumber;
         }
     })
+})
+
+
+const menubar = document.querySelector("#menu_bar");
+const menu = document.querySelector("#menu_list");
+const closeMenu = document.querySelector("#close_menu");
+
+let menuIcon = true;
+
+menubar.addEventListener('click', function navbar() {
+
+    const closeMenu = document.querySelector("#close_menu");
+    menuIcon = !menuIcon;
+    if (menuIcon) {
+        menu.classList.remove("active_menu");
+    } else {
+        menu.classList.add("active_menu");
+        closeMenu.style.opacity = "1";
+    }
+}
+)
+
+
+
+closeMenu.addEventListener('click', function closeBar() {
+    closeMenu.style.opacity = "0";
+    menu.classList.remove("active_menu");
 })
